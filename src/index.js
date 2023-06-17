f02c4cef48935801f9f7fa0fcaa006c555d702b5
AmeyPank <pankantiameya@gmail.com>
2023-06-17 19:44:59 +0530

    React Calculator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById("root"))
+
+// "scripts": {
+//     "test": "jest --no-color 2>output.txt",
+//     "start": "webpack-dev-server --mode development --watch --open --hot",
+//     "build": "webpack --mode production"
+//   },
+
+// "scripts": {
+//     "test": "NODE_OPTIONS=--openssl-legacy-provider jest --no-color 2>output.txt",
+//     "start": "NODE_OPTIONS=--openssl-legacy-provider webpack-dev-server --mode development --watch --open --hot",
+//     "build": "NODE_OPTIONS=--openssl-legacy-provider webpack --mode production"
+//   },
